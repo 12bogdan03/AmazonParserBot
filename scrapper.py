11ed0eab8816b1b144aacaad1a7fe700829e7b58
Bohdan Karvatskyi <bohdan.karvatskyi@cryptaldash.com>
2018-11-24 23:41:10 +0200

    add normal user-agent to selenium

--- scrapper.py
+++ scrapper.py
@@ -2,6 +2,7 @@ import os
 
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 
 from config import logger, BASEDIR
 
@@ -10,9 +11,13 @@ class Scrapper:
     def __init__(self):
         # chrome_options = Options()
         # chrome_options.add_argument("--headless")
+        caps = DesiredCapabilities.PHANTOMJS
+        caps["phantomjs.page.settings.userAgent"] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ' \
+                                                    '(KHTML, like Gecko) Chrome/70.0.3538.77 Safari/537.36'
         self.driver = webdriver.PhantomJS(os.path.join(BASEDIR,
                                                        'drivers',
-                                                       'phantomjs'))
+                                                       'phantomjs'),
+                                          desired_capabilities=caps)
 
     def __del__(self):
         self.driver.close()
